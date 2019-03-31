e0844c1722ddc2115ee2cdf5451fa7679bf7000b
lucerohospina <lucerojhp@gmail.com>
2019-03-31 15:40:14 -0500

    uploading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OpportunitiesComponent } from './pages/opportunities/opportunities.component';
+import { BaseComponent } from './commons/base/base.component';
+import { SidebarComponent } from './commons/sidebar/sidebar.component';
+import { InvestmentsComponent } from './pages/investments/investments.component';
+import { BankAccountsComponent } from './pages/bank-accounts/bank-accounts.component';
+import { DepositsComponent } from './pages/deposits/deposits.component';
+
+const routes : Routes = [
+  { path : 'login', component : LoginComponent },
+  {path: '', component: BaseComponent, children : [
+    {path : '', redirectTo: '/oportunidades', pathMatch: 'full'},
+    {path: 'oportunidades', component: OpportunitiesComponent},
+    {path: 'inversiones', component: InvestmentsComponent},
+    {path: 'cuentas-bancarias', component: BankAccountsComponent},
+    {path: 'depositar-dinero', component: DepositsComponent}
+  ]},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OpportunitiesComponent,
+    BaseComponent,
+    SidebarComponent,
+    InvestmentsComponent,
+    BankAccountsComponent,
+    DepositsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
