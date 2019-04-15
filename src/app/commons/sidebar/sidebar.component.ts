2f03e44e75bf9881818289b1136c9e28ba28db02
AlejandraCP <alejandracp710@gmail.com>
2019-04-14 23:20:49 -0500

    convert svg to fonts

--- src/app/commons/sidebar/sidebar.component.ts
+++ src/app/commons/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +8,7 @@ import { Router } from '@angular/router'
 })
 export class SidebarComponent implements OnInit {
 
-  name = 'Alexander'
+  name = 'Alexander';
 
   constructor(public _router: Router) { }
 
