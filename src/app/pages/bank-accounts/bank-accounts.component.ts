d3d70b667b40ba14c1d8fcd144a7e6a838386afa
AlejandraCP <alejandracp710@gmail.com>
2019-04-19 13:11:01 -0500

    accounts mobile

--- src/app/pages/bank-accounts/bank-accounts.component.ts
+++ src/app/pages/bank-accounts/bank-accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bank-accounts',
@@ -10,28 +11,38 @@ export class BankAccountsComponent implements OnInit {
   bankAccounts = [
     {
       bank: 'BBVA Continental',
-      ammount: 'S/ 14,889,95',
+      account: '43278972318',
       cci: '01135900020018600474',
       currency: 'PEN',
       date: '04/feb/2019',
     },
     {
-      bank: 'BBVA Continental',
-      ammount: 'S/ 14,889,95',
+      bank: 'BCP',
+      account: '43278972318',
       cci: '01135900020018600474',
       currency: 'PEN',
       date: '04/feb/2019',
     },
     {
-      bank: 'BBVA Continental',
-      ammount: 'S/ 14,889,95',
+      bank: 'Scotiabank',
+      account: '43278972318',
       cci: '01135900020018600474',
       currency: 'PEN',
       date: '04/feb/2019',
     }
   ];
 
-  constructor() { }
+  showNavigationArrows = true;
+  showNavigationIndicators = false;
+
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.interval = 0;
+    config.showNavigationIndicators = false;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit() {
   }
