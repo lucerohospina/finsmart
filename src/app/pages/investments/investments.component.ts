2b6d51422c2e056358df8c76f52b35e6daaa6980
AlejandraCP <alejandracp710@gmail.com>
2019-04-19 12:46:04 -0500

    counts mobile

--- src/app/pages/investments/investments.component.ts
+++ src/app/pages/investments/investments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-investments',
@@ -162,7 +163,17 @@ export class InvestmentsComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  showNavigationArrows = true;
+  showNavigationIndicators = false;
+  
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.interval = 0;
+    config.showNavigationIndicators = false;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit() {
   }
