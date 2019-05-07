f40025ae938fb7dd26afeecae897f48e6fed0993
elizalc <elizabethalcala28@gmail.com>
2019-05-07 07:54:08 -0500

    Add modals and toggle for deposits view

--- src/app/pages/deposits/deposits.component.ts
+++ src/app/pages/deposits/deposits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-deposits',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposits.component.css']
 })
 export class DepositsComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title',  size: 'sm', centered: true, windowClass: 'loginModal'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    });
+  }
 
   ngOnInit() {
   }
